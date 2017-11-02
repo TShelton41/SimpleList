b9633fa7a377a839610f48f9390e972ea94f9cb3
TS <ts@Todds-iMac.local>
2017-11-01 20:33:58 -0400

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController} from 'ionic-angular';
+import {DataProvider} from "../../providers/data/data";
 
+@IonicPage()
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    homelists:any;
 
-  }
+    constructor(public navCtrl: NavController, public data:DataProvider) {
+
+    }
+
+    ionViewDidLoad(){
+        this.homelists = this.data.lists;
+    }
 
 }
---
+++ src/providers/data/data.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the DataProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class DataProvider {
+
+    lists: any = [
+        {item: "milk"},
+        {item: "cheese"},
+        {item: "juice"},
+        {item: "meat"},
+        {item: "veggies"},
+        {item: "cheetos"}
+    ];
+
+    constructor() {
+        console.log('Hello DataProvider Provider');
+    }
+
+}
